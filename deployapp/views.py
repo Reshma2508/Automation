44e96fe5fbe6bb8f966d3e5031e62c2b243918bd
Reshma <reshunikam1998@gmail.com>
2023-07-29 11:58:16 +0530

    PRoject addded to git

---
+++ deployapp/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(r):
+    return HttpResponse("<h1>Hi Its Deployment_29Jul Project , We are deploying it using AWS, jenkins, Docker, Git</h1>")
